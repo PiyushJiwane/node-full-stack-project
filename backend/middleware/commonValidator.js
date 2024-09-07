271c0c104b52c5d6f0cc6664c48a97f73256e1e9
PiyushJiwane <jiwanepiyush@gmail.com>
2024-09-07 12:00:03 +0530

    added : all the express-validators req for body, param and query are moved to commonValidator.js

---
+++ backend/middleware/commonValidator.js
@@ -0,0 +1,27 @@
+const { check, body, validationResult, query } = require('express-validator');
+
+const username_validBody = body('username').trim().isLength({ min: 1 }).withMessage('Name must be specified')
+
+const email_validBody = body('email').isEmail().normalizeEmail().withMessage('Invalid email')
+
+const email_validQuery = query('email').isEmail().normalizeEmail().withMessage('Invalid email')
+
+const password_validBody = body('password')
+    .isLength({ min: 8 }).withMessage('Password must be at least 8 characters long')
+    .matches(/[a-z]/).withMessage('Password must contain at least one lowercase letter')
+    .matches(/[A-Z]/).withMessage('Password must contain at least one uppercase letter')
+    .matches(/\d/).withMessage('Password must contain at least one number')
+    .matches(/[!@#$%^&*(),.?":{}|<>]/).withMessage('Password must contain at least one special character')
+
+const otp_validBody = body('otp').isLength({ min: 4, max: 4 })
+    .withMessage('OTP must be exactly 4 digits')
+    .isNumeric()
+    .withMessage('OTP must be numeric')
+
+module.exports={
+    username_validBody,
+    email_validBody,
+    password_validBody,
+    otp_validBody,
+    email_validQuery
+}
