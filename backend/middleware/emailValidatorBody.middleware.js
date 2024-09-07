048d3cbeed3e18e3486f56dcff7c363ff2897eb9
PiyushJiwane <jiwanepiyush@gmail.com>
2024-09-07 12:01:28 +0530

    added : this middleware used to validate the email id's comible to check whether user exit in the db or not

---
+++ backend/middleware/emailValidatorBody.middleware.js
@@ -0,0 +1,20 @@
+const { body, validationResult } = require('express-validator');
+const logger = require('../logging/logger');
+const { email_validBody, email_validQuery } = require('./commonValidator');
+
+
+const emailValidatorBody=[
+    email_validBody,
+    
+    // After validation, check for errors
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            logger.error(errors.array()[0].msg)
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next(); // If no errors, proceed to the next middleware (controller)
+    }
+]
+
+module.exports=emailValidatorBody
