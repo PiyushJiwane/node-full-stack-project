285b3baf59e73d4396fa04dbbfbdb2df64a26c8d
PiyushJiwane <jiwanepiyush@gmail.com>
2024-09-07 14:50:58 +0530

    updated : login route updated for forgetPassword api

--- backend/routes/login.route.js
+++ backend/routes/login.route.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const {updateUser, deleteUser, login } = require("../controller/login.controller");
+const {updateUser, deleteUser, login, forgetPassword } = require("../controller/login.controller");
 const loginValidator = require("../middleware/loginValidator");
+const forgetPassValidator = require("../middleware/forgetPassValidator.middleware");
 
 const login_route = express.Router()
 const initPath="/todo/api/v1"
@@ -8,5 +9,6 @@ const initPath="/todo/api/v1"
 login_route.post(`${initPath}/login`, loginValidator, login)
 login_route.put(`${initPath}/updateUser/:userId`, updateUser)
 login_route.delete(`${initPath}/deleteUser/:userId`, deleteUser)
+login_route.post(`${initPath}/forgetPassword`,forgetPassValidator,forgetPassword)
 
 module.exports = login_route
