2bfddaeff85b6bcc838fd7181f3809fe7aa93b0b
PiyushJiwane <jiwanepiyush@gmail.com>
2024-09-10 16:07:24 +0530

    updated : todo route updated for retriveTodo

--- backend/routes/todo.route.js
+++ backend/routes/todo.route.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const saveTodo = require("../controller/todo.controller");
+const {saveTodo, retriveTodo} = require("../controller/todo.controller");
 const userIdValidator = require("../middleware/userIdValidator");
 
 const todo_route = express.Router()
 const initPath="/todo/api/v1"
 
+todo_route.get(`${initPath}/todo/:userId`,userIdValidator, retriveTodo)
 todo_route.post(`${initPath}/todo/:userId`,userIdValidator, saveTodo)
 
 module.exports = todo_route
