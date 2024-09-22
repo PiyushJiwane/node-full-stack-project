33f2e9146776f737387b5d560a8060607502b064
PiyushJiwane <jiwanepiyush@gmail.com>
2024-09-22 17:08:35 +0530

    updated : new middleware verifyJWT added to check request is valid and authenticated or not

--- backend/routes/todo.route.js
+++ backend/routes/todo.route.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const {saveTodo, retriveTodo, inActiveTodo} = require("../controller/todo.controller");
 const userIdValidator = require("../middleware/userIdValidator");
+const verifyJWT = require("../middleware/verifyJWT.middleware");
 
 const todo_route = express.Router()
 const initPath="/todo/api/v1"
 
-todo_route.get(`${initPath}/todo/:userId`,userIdValidator, retriveTodo)
-todo_route.post(`${initPath}/todo/:userId`,userIdValidator, saveTodo)
-todo_route.put(`${initPath}/inActive/todo/:userId`,userIdValidator, inActiveTodo)
+todo_route.get(`${initPath}/todo/:userId`,[userIdValidator,verifyJWT], retriveTodo)
+todo_route.post(`${initPath}/todo/:userId`,[userIdValidator,verifyJWT], saveTodo)
+todo_route.put(`${initPath}/inActive/todo/:userId`,[userIdValidator,verifyJWT], inActiveTodo)
 
 module.exports = todo_route
