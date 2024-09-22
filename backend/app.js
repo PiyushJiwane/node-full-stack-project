cf528783531bcd727ec270581aec369a7adf6167
PiyushJiwane <jiwanepiyush@gmail.com>
2024-09-22 16:50:52 +0530

    udpated : corsOptions added in the cors() middleware and refreshToken_route added

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,9 @@ const YAML = require("yamljs");
 const path = require("path");
 const cors = require('cors');
 const todo_route = require("./routes/todo.route");
+const crosOptions = require("./config/cors");
+const cookieParser = require("cookie-parser");
+const refreshToken_route = require("./routes/refreshToken.route");
 
 const swaggerDocument = YAML.load(path.join(__dirname, "../backend/docs/output.yml"));
 
@@ -18,12 +21,18 @@ const PORT=process.env.PORT || 3000
 
 app.use(morgan('tiny'))
 app.use(express.json())
-app.use(cors());
+app.use(cors(crosOptions));
+app.use(cookieParser())
 
 app.use("/",signup_route)
 app.use("/",login_route)
 app.use("/",todo_route)
+app.use("/",refreshToken_route)
 app.use('/docs', cors(),swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    res.status(500).send(err.msg)
+})
 
 const startServer = async () => {
     try {
@@ -36,9 +45,9 @@ const startServer = async () => {
     }
 }
 
-startServer()
-
 // Handle 404 errors for undefined routes
 app.all("*", (req, res) => {
-    res.status(404).json({ error: "404 Not Found !!!" });
+    res.status(404).json({ error: "404 Not Found... !!!" });
 });
+startServer()
+
