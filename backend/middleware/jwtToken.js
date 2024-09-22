799bb52990751839ccb3110cf2fbd3806d90ef2d
PiyushJiwane <jiwanepiyush@gmail.com>
2024-09-22 17:01:27 +0530

    udpated : generateJWTRefreshToken() fn is used to genearte the new refreshToken

--- backend/middleware/jwtToken.js
+++ backend/middleware/jwtToken.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken")
 require('dotenv/config')
 
 const JWT_SECRETE_KEY = process.env.JWT_SECRETE_KEY
+const JWT_REFRESHTOKEN_SECRETE_KEY = process.env.JWT_REFRESHTOKEN_SECRETE_KEY
 
 const generateJWTToken = async (result) => {        
     // Calculate the timestamp for midnight
@@ -20,24 +21,22 @@ const generateJWTToken = async (result) => {
     return jwt_token
 }
 
-const isJWTTokenValid = (token) => {
-    const date = new Date();
-    // Verifying the JWT token 
-    jwt.verify(token, JWT_SECRETE_KEY, function (err, decoded) {
-        if (err) {
-            console.log(`${date.getHours()}:${date.getMinutes()}
-                                       :${date.getSeconds()}`);
-            console.log(err);
-            return true
-        }
-        else {
-            console.log(`${date.getHours()}:${date.getMinutes()}
-                                       :${date.getSeconds()}`);
-            console.log("Token verifified successfully");
-            return false
-        }
-    });
+const generateJWTRefreshToken = async (result) => {        
+    // Calculate the timestamp for midnight
+    let midnight = new Date();
+    midnight.setHours(24, 0, 0, 0); // Set time to 00:00:00 of the next day
+
+    // Calculate the expiry time in seconds from now until midnight
+    let expirySeconds = Math.floor((midnight.getTime() - Date.now()) / 1000);
+
+    const jwt_token = await jwt.sign({
+        _id: result._id,
+        u_name: result.username
+    }, JWT_REFRESHTOKEN_SECRETE_KEY, {
+        expiresIn: "7d"
+    })
+    return jwt_token
 }
 
 
-module.exports = generateJWTToken
+module.exports = {generateJWTToken, generateJWTRefreshToken}
