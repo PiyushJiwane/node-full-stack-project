908d14c3947fca2ab16e9573567d8e7139bf8c43
PiyushJiwane <jiwanepiyush@gmail.com>
2024-10-01 11:43:41 +0530

    added : this page contians the code for login acitivity

---
+++ frontend/src/components/LoginPage.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import { useLoginMutation } from '../redux_store/loginSlice'
+import { setCredentials } from '../redux_store/authSlice'
+import { useDispatch } from 'react-redux'
+import { Outlet, useNavigate } from 'react-router-dom'
+
+function LoginPage() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const [login] = useLoginMutation()
+    const dispatch = useDispatch()
+    const navigate=useNavigate()
+
+    const onSubmitHandler = async (e) => {
+        e.preventDefault()
+        const jwt_token = await login({ email, password })
+        console.log(`onSubmitHandler : ${JSON.stringify(jwt_token)}`);
+        dispatch(setCredentials({ ...jwt_token.data, email }))
+        navigate("/dashobard")
+    }
+
+    const onForgetPassword = () => {
+        console.log("onForgetPassword");
+        navigate("/forget-password")
+    }
+
+    const onSignup = () => {
+        console.log("onSignup");
+        navigate("/signup")
+    }
+
+    return (
+        <>
+            <form onSubmit={onSubmitHandler} method='POST'>
+                <label>Email : </label>
+                <input name='email' id='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+                <br /><br />
+                <label>Password : </label>
+                <input name='password' id='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                <br /><br />
+                <button name='login' type='submit' disabled={!email || !password} >Login</button>
+            </form>
+            <br />
+            <button type='button' onClick={onForgetPassword}>Forget Password</button>
+            <button type='button' onClick={onSignup}>Signup</button>
+  
+        </>
+    )
+}
+
+export default LoginPage
