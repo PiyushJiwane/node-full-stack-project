1b508eb7341db84bd594bb242034ab312b6804ce
PiyushJiwane <jiwanepiyush@gmail.com>
2024-10-01 12:17:00 +0530

    routes introduced

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,17 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Main from './components/Main';
 import { Provider } from 'react-redux';
 import { store } from './redux_store/store';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <CssBaseline />
     <Provider store={store}>
-      <Main />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<Main />} />
+      </Routes>
+      </BrowserRouter>
     </Provider>
 
   </StrictMode>
