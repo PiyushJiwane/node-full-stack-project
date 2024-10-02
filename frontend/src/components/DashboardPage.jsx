bde56505acf80f5708db299e1a956476d6258125
PiyushJiwane <jiwanepiyush@gmail.com>
2024-10-02 20:47:16 +0530

    updated : code done for saveTodo api

--- frontend/src/components/DashboardPage.jsx
+++ frontend/src/components/DashboardPage.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
 import { Outlet } from 'react-router-dom'
+import { useSaveTodoMutation } from '../redux_store/todoSlice'
 
 function DashboardPage() {
+  const [title, setTitle] = useState('')
+  const [desc, setDesc] = useState('')
+
+  const id = useSelector(state => state.auth._id)
+  
+  const [saveTodo,{isSuccess,data}] = useSaveTodoMutation()
+  
+  const onSubmitHandler = async (e) => {
+    e.preventDefault()
+    console.log("dashboard");
+    console.log(`id : ${id}`);
+    await saveTodo({
+      userId:id,
+      title,
+      desc
+    }).unwrap()
+    setTitle('')
+    setDesc('')
+}
+
   return (
-      <>DashboardPage
-      </>
+    <>DashboardPage
+  <form onSubmit={onSubmitHandler} method='POST'>
+                <label>Title : </label>
+                <input name='title' id='title' value={title} onChange={(e) => setTitle(e.target.value)} />
+                <br /><br />
+                <label>Desc : </label>
+                <input name='desc' id='desc' value={desc} onChange={(e) => setDesc(e.target.value)} />
+                <br /><br />
+                <button name='login' type='submit' disabled={!title || !desc} >Add</button>
+            </form>
+            {
+            isSuccess && data && (<p>save</p>)
+            }
+    </>
   )
 }
 
