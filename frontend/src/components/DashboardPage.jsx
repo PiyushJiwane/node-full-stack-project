db0fde48594bdff601f5091ec8124e47b7439cde
PiyushJiwane <jiwanepiyush@gmail.com>
2024-10-03 13:07:27 +0530

    updated : code updated to retrive the todo list creadted for a user

--- frontend/src/components/DashboardPage.jsx
+++ frontend/src/components/DashboardPage.jsx
@@ -1,20 +1,20 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { Outlet } from 'react-router-dom'
-import { useSaveTodoMutation } from '../redux_store/todoSlice'
+import { useRetriveTodoQuery, useSaveTodoMutation } from '../redux_store/todoSlice'
 
 function DashboardPage() {
   const [title, setTitle] = useState('')
   const [desc, setDesc] = useState('')
 
   const id = useSelector(state => state.auth._id)
+  console.log(`DashboardPage : id : ${id}`);
   
-  const [saveTodo,{isSuccess,data}] = useSaveTodoMutation()
+  const [saveTodo, { isSuccess, data }] = useSaveTodoMutation()
+  const { data:retriveTodo,isSuccess:isSuccessTodo } = useRetriveTodoQuery(id)  
   
   const onSubmitHandler = async (e) => {
     e.preventDefault()
-    console.log("dashboard");
-    console.log(`id : ${id}`);
     await saveTodo({
       userId:id,
       title,
@@ -37,7 +37,23 @@ function DashboardPage() {
             </form>
             {
             isSuccess && data && (<p>save</p>)
-            }
+      }
+      {isSuccessTodo ? (
+        <ul>
+          {/* Map through the retrieved to-do items and display them */}
+          {retriveTodo?.map((item) => (
+            <li key={item._id}>
+              <h4>Title: {item.todo.title}</h4>
+              <p>Description: {item.todo.desc}</p>
+              <p>
+                <strong>Created At:</strong> {new Date(item.todo.createdAt).toLocaleString()}
+              </p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Loading...</p>
+      )}
     </>
   )
 }
