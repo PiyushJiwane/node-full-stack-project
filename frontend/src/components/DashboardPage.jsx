a6fc8aba02bdd8c1aa1c84acaa169cc3f6711f98
PiyushJiwane <jiwanepiyush@gmail.com>
2024-10-06 13:32:32 +0530

    updated : by using useNavigate() hook we are passing data to new page if user wants to update the data

--- frontend/src/components/DashboardPage.jsx
+++ frontend/src/components/DashboardPage.jsx
@@ -1,42 +1,50 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 import { useRetriveTodoQuery, useSaveTodoMutation } from '../redux_store/todoSlice'
 
 function DashboardPage() {
   const [title, setTitle] = useState('')
   const [desc, setDesc] = useState('')
 
+  const navigate = useNavigate()
+
   const id = useSelector(state => state.auth._id)
   console.log(`DashboardPage : id : ${id}`);
-  
+
   const [saveTodo, { isSuccess, data }] = useSaveTodoMutation()
-  const { data:retriveTodo,isSuccess:isSuccessTodo } = useRetriveTodoQuery(id)  
-  
+  const { data: retriveTodo, isSuccess: isSuccessTodo } = useRetriveTodoQuery(id)
+
   const onSubmitHandler = (e) => {
     e.preventDefault()
-     saveTodo({
-      userId:id,
+    saveTodo({
+      userId: id,
       title,
       desc
     }).unwrap()
     setTitle('')
     setDesc('')
-}
+  }
+
+  const onSubmitUpdateTodoHandler = (e, item) => {
+    e.preventDefault()
+    console.log(item);
+    navigate("/update-todo", { state: { item } })
+  }
 
   return (
     <>DashboardPage
-  <form onSubmit={onSubmitHandler} method='POST'>
-                <label>Title : </label>
-                <input name='title' id='title' value={title} onChange={(e) => setTitle(e.target.value)} />
-                <br /><br />
-                <label>Desc : </label>
-                <input name='desc' id='desc' value={desc} onChange={(e) => setDesc(e.target.value)} />
-                <br /><br />
-                <button name='login' type='submit' disabled={!title || !desc} >Add</button>
-            </form>
-            {
-            isSuccess && data && (<p>save</p>)
+      <form onSubmit={onSubmitHandler} method='POST'>
+        <label>Title : </label>
+        <input name='title' id='title' value={title} onChange={(e) => setTitle(e.target.value)} />
+        <br /><br />
+        <label>Desc : </label>
+        <input name='desc' id='desc' value={desc} onChange={(e) => setDesc(e.target.value)} />
+        <br /><br />
+        <button name='login' type='submit' disabled={!title || !desc} >Add</button>
+      </form>
+      {
+        isSuccess && data && (<p>save</p>)
       }
       {isSuccessTodo ? (
         <ul>
@@ -48,6 +56,7 @@ function DashboardPage() {
               <p>
                 <strong>Created At:</strong> {new Date(item.todo.createdAt).toLocaleString()}
               </p>
+              <button onClick={(e) => { onSubmitUpdateTodoHandler(e, item) }}>Update</button>
             </li>
           ))}
         </ul>
