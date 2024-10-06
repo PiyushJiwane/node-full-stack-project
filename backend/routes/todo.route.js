6460b2706887991bf71451f7069f094f52d74674
PiyushJiwane <jiwanepiyush@gmail.com>
2024-10-06 13:30:11 +0530

    updated : todo route updated for updateTodo() fn

--- backend/routes/todo.route.js
+++ backend/routes/todo.route.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {saveTodo, retriveTodo, inActiveTodo} = require("../controller/todo.controller");
+const {saveTodo, retriveTodo, inActiveTodo,updateTodo} = require("../controller/todo.controller");
 const userIdValidator = require("../middleware/userIdValidator");
 const verifyJWT = require("../middleware/verifyJWT.middleware");
 
@@ -9,5 +9,6 @@ const initPath="/todo/api/v1"
 todo_route.get(`${initPath}/todo/:userId`,[userIdValidator,verifyJWT], retriveTodo)
 todo_route.post(`${initPath}/todo/:userId`,[userIdValidator,verifyJWT], saveTodo)
 todo_route.put(`${initPath}/inActive/todo/:userId`,[userIdValidator,verifyJWT], inActiveTodo)
+todo_route.put(`${initPath}/update/todo/:userId`,[userIdValidator,verifyJWT], updateTodo)
 
 module.exports = todo_route
